bd21b1dbd9f39ea28ba7f36f21ab35f37a677884
Jeff Duke <jeff.duke@gmail.com>
2017-11-20 18:45:30 -0700

    add ideas to page

--- src/App.js
+++ src/App.js
@@ -16,24 +16,57 @@ class App extends Component {
     this.setState({ [name]: value });
   }
 
+  addNewIdea() {
+    const { title, body, ideas } = this.state;
+    const idea = {
+      title,
+      body,
+      quality: 1,
+      id: Date.now(),
+    };
+
+    ideas.unshift(idea);
+    this.setState({ ideas: ideas });
+  }
+
   render() {
+    const { ideas } = this.state;
+    console.log(ideas);
     return (
       <div className="App">
-        <label htmlFor="title">Title</label>
-        <input
-          onChange={e => this.updateIdeaInfo(e)}
-          name="title"
-          id="title"
-          type="text"
-        />
-        <label htmlFor="body" />
-        <input
-          onChange={e => this.updateIdeaInfo(e)}
-          name="body"
-          id="body"
-          type="text"
-        />
-        <button>Add Idea</button>
+        <section>
+          <label htmlFor="title">Title</label>
+          <input
+            onChange={e => this.updateIdeaInfo(e)}
+            name="title"
+            id="title"
+            type="text"
+          />
+          <label htmlFor="body">Body</label>
+          <input
+            onChange={e => this.updateIdeaInfo(e)}
+            name="body"
+            id="body"
+            type="text"
+          />
+          <button onClick={() => this.addNewIdea()}>Add Idea</button>
+        </section>
+        <section>
+          {ideas.map(idea => {
+            return (
+              <div key={idea.id}>
+                <h1>{idea.title}</h1>
+                <h2>{idea.body}</h2>
+                <p>
+                  quality:{' '}
+                  {idea.quality === 1
+                    ? 'swill'
+                    : idea.quality === 2 ? 'plausible' : 'genius'}
+                </p>
+              </div>
+            );
+          })}
+        </section>
       </div>
     );
   }
